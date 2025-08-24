03e4b6624fcfcb7a084f4189ff7c6dee599e6c4c
git <gula16@itu.edu.tr>
2025-08-24 19:08:41 +0300

    EnumValidator added

---
+++ spring-brokerage-project/stock-orders/src/main/java/org/brokage/stockorders/utility/ValidEnum.java
@@ -0,0 +1,17 @@
+package org.brokage.stockorders.utility;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = EnumValidator.class)
+@Target({ ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidEnum {
+    String message() default "Invalid value. Allowed values are: {allowedValues}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<? extends Enum<?>> enumClass();
+}
