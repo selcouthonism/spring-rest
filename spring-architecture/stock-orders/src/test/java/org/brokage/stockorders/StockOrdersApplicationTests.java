1e6dda2e74e767b315cd51282044f5a43340d174
git <gula16@itu.edu.tr>
2025-08-29 11:14:42 +0300

    Spring Project created

---
+++ spring-architecture/stock-orders/src/test/java/org/brokage/stockorders/StockOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.brokage.stockorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
